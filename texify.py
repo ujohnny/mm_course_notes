589e5c6af940d73256435ebb775965f040551cdd
B@rmaley.exe <sobola2@RUENSOBOLA2L1C.corp.emc.com>
2013-09-20 01:04:44 +0400

    A bit of useless refactoring

--- texify.py
+++ texify.py
@@ -11,9 +11,13 @@ import subprocess
 path = sys.argv[1]
 files = glob.glob(path + "*.md")
 
+def process(out):
+	out = out.replace("\r","").replace("`","'")
+	return out
+
 output = ""
 for f in files:
-	output += subprocess.check_output("pandoc -f markdown -t latex '" + f + "'", shell=True).replace("\r","").replace("`","'")
+	output += process(subprocess.check_output("pandoc -f markdown -t latex '" + f + "'", shell=True))
 
 template = open("template.latex").read()
 print template.replace("[:BODY:]", output)
